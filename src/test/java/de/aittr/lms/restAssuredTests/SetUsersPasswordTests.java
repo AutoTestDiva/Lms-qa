fadeffe0d433f3a63137a92136019676c413e080
Liliia Baieva <555blv@gmail.com>
2023-11-11 00:37:52 +0200

    SetUsersPasswordTests.java with setUsersPasswordPositiveTest()

--- src/test/java/de/aittr/lms/restAssuredTests/SetUsersPasswordTests.java
+++ src/test/java/de/aittr/lms/restAssuredTests/SetUsersPasswordTests.java
@@ -1,7 +1,20 @@
 package de.aittr.lms.restAssuredTests;
 
-public class SetUsersPassword extends TestBaseRA{
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
 
+import java.sql.SQLException;
+
+public class SetUsersPasswordTests extends TestBaseRA{
+
+//    @Test(dependsOnMethods = "registerUserByAdminPositiveTest")
+    @Test
+    public void setUsersPasswordPositiveTest() throws SQLException {
+        user.setPasswordByEmail("lilu2@mail.com", "Qwerty123!").then()
+                .assertThat().statusCode(200);
+
+    }
 
 
 }
