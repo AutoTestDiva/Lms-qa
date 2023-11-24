f8b4a669f8a020039eb0df8a433b5e3e90ef96aa
Liliia Baieva <555blv@gmail.com>
2023-11-24 16:22:35 +0200

    Create DownloadVideoTests.java to get posibilites to download video from web site

--- src/test/java/de/aittr/lms/UITests/DownloadVideoTests.java
+++ src/test/java/de/aittr/lms/UITests/DownloadVideoTests.java
@@ -1,2 +1,28 @@
-package de.aittr.lms.fwUI;public class DownloadVideoTests {
+package de.aittr.lms.UITests;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class DownloadVideoTests extends TestBaseUI{
+
+        @BeforeMethod
+        public void precondition(){
+                app.getUserUI().loginWithData("student@mail.com", "Qwerty123!");
+        }
+//        Предусловие: залогиниться как пользователь
+//        с данными {email: “student@mail.com“, password: “Qwerty123!“, cohort: “Cohort 34.2“}
+
+        @Test
+        public void downloadVideoTest(){
+//                 TODO downloadVideoTest
+                app.getLesson().selectStudentGroup("Cohort 34.2");
+                app.getLesson().selectModule("basic_programming");
+                app.getLesson().selectLesson("lesson_30");
+                app.getLesson().clickOnVideoPart();
+//        На видео кликаю правой кнопкой мышки и выбираю “Загрузить видео как“
+                // todo click with mouse
+//        Ожидаемый результат: Невозможно загрузить видео, при клике правой кнопкой мышки отсутствует “Загрузить видео как“.
+                // TODO Assert, learn how can verify download on PC
+        }
+
 }
