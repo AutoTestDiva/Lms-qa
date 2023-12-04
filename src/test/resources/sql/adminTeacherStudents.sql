619b0718097fa91f418259fe468ae87687f6a098
Liliia Baieva <555blv@gmail.com>
2023-12-04 19:11:24 +0100

    create sql scripts to add cohorts and to create main user as Admin, Teacher, Student and Student2

---
+++ src/test/resources/sql/adminTeacherStudents.sql
@@ -0,0 +1,16 @@
+INSERT INTO account (country, email, password_hash, first_name, last_name, is_active, phone, role, state)
+    VALUES ("Germany","admin@mail.com","$2a$10$ZRBVssy0mqXLL1Wv80LaROywrAWdW1mo0.8IAfKrxiswmcBlE1bai",
+            "Jhon","Admin",true,"+490577777777","ADMIN","CONFIRMED");
+
+INSERT INTO account (country, email, password_hash, first_name, last_name, is_active, phone, role, state)
+    VALUES ("Germany","teacher@mail.com","$2a$10$lIVNxb6bl2Uas/bOz6LdAetYzzSfTRIFx4iJS9fR5u1XqkLujcApS","Teacher",
+            "Testteach",true,"+401871234567","TEACHER","CONFIRMED");
+
+INSERT INTO account (country, email, password_hash, first_name, last_name, is_active, phone, role, state)
+    VALUES ("Germany","student2@mail.com","$2a$10$humjffaz25vyY1zmB2FwuONfXnIuN7AKyTJ4nhBAzXG7WfMeLyc9i","Student",
+            "Test",true,"+490571234567","STUDENT","CONFIRMED",41);
+
+INSERT INTO account (country, email, password_hash, first_name, last_name, is_active, phone, role, state)
+    VALUES ("Germany","student@mail.com","$2a$10$9QI9ukaxgJTEYMTeuezGKundlfK9pc9WiHmNyQQvxmxaaNyF9y4ca","Student",
+            "Test",true,"+490571234567","STUDENT","CONFIRMED",39);
+
