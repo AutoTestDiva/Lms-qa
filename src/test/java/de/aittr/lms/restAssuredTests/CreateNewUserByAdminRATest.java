33140628bee8d114a486388e04b34e7ecb047f66
Nataliia <pnata1978@gmail.com>
2024-06-12 13:17:56 +0200

    test1

--- src/test/java/de/aittr/lms/restAssuredTests/CreateNewUserByAdminRATest.java
+++ src/test/java/de/aittr/lms/restAssuredTests/CreateNewUserByAdminRATest.java
@@ -35,10 +35,12 @@ public class CreateNewUserByAdminRATest extends  TestBaseRA{
                 .then().log().all().assertThat().statusCode(201);
     }
 
+
     @Test    public void createNewUserWithoutAdminNegativeTest() {
         given().contentType(ContentType.JSON).body(newUser).when().post("/users/create-user")
                 .then().log().all().assertThat().statusCode(401);
     }
+
     @Test
     public void createUserWithWrongRoleByAdminNegativeTest() {
         NewUserWithRoleDto notValidUser = user.userWithRoleBuilder("Cohort 99", mail, "Testerin", "Test",
@@ -93,4 +95,5 @@ public class CreateNewUserByAdminRATest extends  TestBaseRA{
         given().contentType(ContentType.JSON).body(newUser).cookie(cookie).when().post("/users/create-user")
                 .then().log().all().assertThat().statusCode(409);
     }
+
 }
